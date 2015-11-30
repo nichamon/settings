0cf7523024c7d37cbf5a01cd3c6a227cc9c6b62f
Narate Taerat <narate@ogc.us>
2015-11-30 14:43:44 -0600

    bash stuff

---
+++ profile.d/prompt.sh
@@ -0,0 +1,22 @@
+parent=`ps -o cmd --no-headers $PPID | awk '{ print $1 }'`
+if (( $UID )); then
+	# This is regular user
+	left='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
+else
+	# This is root
+	left='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
+fi
+#right="\\[\\033[01;31m\\][$parent]"
+right="\\[\\033[01;31m\\][$parent]"
+
+myprompt() {
+	PS1=$(printf "\n%*s\r%s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left")
+}
+
+export PROMPT_COMMAND=myprompt
+export PYTHONSTARTUP=$HOME/.pyrc
+export DISPLAY=:0
+alias vim='vim -X'
+
+# use vi mode
+set -o vi

15260b0abf135d908b9be1114d1b367c30619f35
Narate Taerat <narate.github@gmail.com>
2016-05-28 23:09:24 -0500

    console terminal-sexy

---
+++ profile.d/terminal-sexy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+if [ "$TERM" = "linux" ]; then
+# backup.
+#  /bin/echo -e "
+#  \e]P0282a2e
+#  \e]P1a54242
+#  \e]P28c9440
+#  \e]P3de935f
+#  \e]P45f819d
+#  \e]P585678f
+#  \e]P65e8d87
+#  \e]P7707880
+#  \e]P8373b41
+#  \e]P9cc6666
+#  \e]PAb5bd68
+#  \e]PBf0c674
+#  \e]PC81a2be
+#  \e]PDb294bb
+#  \e]PE8abeb7
+#  \e]PFc5c8c6
+#  "
+  /bin/echo -e "
+  \e]P0282a2e
+  \e]P1a54242
+  \e]P28c9440
+  \e]P3de935f
+  \e]P45f819d
+  \e]P585678f
+  \e]P65e8d87
+  \e]P7c5c8c6
+  \e]P8373b41
+  \e]P9cc6666
+  \e]PAb5bd68
+  \e]PBf0c674
+  \e]PC81a2be
+  \e]PDb294bb
+  \e]PE8abeb7
+  \e]PFc5c8c6
+  "
+  # get rid of artifacts
+  clear
+fi

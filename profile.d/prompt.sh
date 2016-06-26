524de9507e3f6242b28b4a019197f5a32a58bbe4
Narate Taerat <narate.github@gmail.com>
2016-06-25 20:27:25 -0500

    prompt.sh takes care only for prompt stuff

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -1,24 +1,21 @@
-parent=`ps -o cmd --no-headers $PPID | awk '{ print $1 }'`
-if (( $UID )); then
-	# This is regular user
-	left='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
-else
-	# This is root
-	left='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
-fi
-#right="\\[\\033[01;31m\\][$parent]"
-right="\\[\\033[01;31m\\][$parent]"
-
 myprompt() {
+	if test -z "$parent"; then
+		parent=`ps -o cmd --no-headers $PPID | awk '{ print $1 }'`
+	fi
+	if test -z "$left"; then
+		if (( $UID )); then
+			# This is regular user
+			left='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
+		else
+			# This is root
+			left='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\h\[\033[01;34m\] \w'
+		fi
+	fi
+
+	right="\\[\\033[01;31m\\][$parent]"
 	echo -ne "\033]0;${USER}@${HOSTNAME}:$PWD\007"
 	PS1=$(printf "\n%*s\r%s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left")
 }
 
 export -f myprompt
 export PROMPT_COMMAND=myprompt
-export PYTHONSTARTUP=$HOME/.pyrc
-export DISPLAY=:0
-alias vim='vim -X'
-
-# use vi mode
-set -o vi
---
+++ profile.d/vim-ish.sh
@@ -0,0 +1,6 @@
+alias vim='vim -X'
+
+# use vi mode
+set -o vi
+
+export EDITOR=vim

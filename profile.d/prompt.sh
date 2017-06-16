35379e2fbe3ad1e687b721080459666535ecc56c
Narate Taerat <narate.github@gmail.com>
2017-06-16 11:14:43 -0500

    profile.d/prompt.sh - add git branch to the prompt

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -12,9 +12,12 @@ myprompt() {
 		fi
 	fi
 
+	_branch="$(git symbolic-ref HEAD --short 2>/dev/null || echo '--')"
+	_git_sym_ref="\\033[0;32m(git-branch: $_branch)"
+
 	right="\\[\\033[01;31m\\][$parent]"
 	echo -ne "\033]0;${USER}@${HOSTNAME}:$PWD\007"
-	PS1=$(printf "\n%*s\r%s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left")
+	PS1=$(printf "\n%*s\r%s\n%*s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left" "$((COLUMNS + 10))" "$_git_sym_ref")
 }
 
 export -f myprompt

d3dd14e90e4da8bf7583375ae416a542cd08de0f
Narate Taerat <narate.github@gmail.com>
2017-11-28 06:19:29 -0600

    bash prompt - use left-justified

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -17,7 +17,8 @@ myprompt() {
 
 	right="\\[\\033[01;31m\\][$parent]"
 	echo -ne "\033]0;${USER}@${HOSTNAME}:$PWD\007"
-	PS1=$(printf "\n%*s\r%s\n%*s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left" "$((COLUMNS + 10))" "$_git_sym_ref")
+	#PS1=$(printf "\n%*s\r%s\n%*s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$((COLUMNS + 15))" "$right" "$left" "$((COLUMNS + 10))" "$_git_sym_ref")
+	PS1=$(printf "\n%s %s\n%s\n\\[\\033[01;34m\\]\$\\[\\033[00m\\] " "$right" "$_git_sym_ref" "$left" )
 }
 
 export -f myprompt

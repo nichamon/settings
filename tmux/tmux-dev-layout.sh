9d2aed25204abff52570cd519b6d24cfc946bb2d
Narate Taerat <narate.github@gmail.com>
2018-12-04 10:23:21 -0600

    Change tmux-new-dev-window to tmux-dev-layout

--- tmux/tmux-dev-layout.sh
+++ tmux/tmux-dev-layout.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-tmux new-window
+P=$(tmux list-panes | wc -l)
+
+if [[ $P != 1 ]]; then
+	echo "This script must be used on a tmux window with one pane"
+	exit -1
+fi
 
 tmux split-window -t 0 -h -l 80
 tmux split-window -t 0 -h -l 80

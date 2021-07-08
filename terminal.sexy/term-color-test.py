e7546299beeea9e972e3cf9bc9a20a4ba3bbb00f
Narate Taerat <narate.github@gmail.com>
2021-07-07 21:15:30 -0500

    term-color-test.py to print color code

--- terminal.sexy/term-color-test.py
+++ terminal.sexy/term-color-test.py
@@ -6,5 +6,6 @@ BG_LIST = range(40,48)
 for bld in ["", 1]:
     for fg in FG_LIST:
         for bg in BG_LIST:
-            print(" \033[{bld};{fg};{bg}mWord\033[0m".format(**locals()), end="")
+            print(" \033[{bld};{fg};{bg}m{bld:1};{fg};{bg}\033[0m".format(**locals()), end="")
+        print("")
         print("")
